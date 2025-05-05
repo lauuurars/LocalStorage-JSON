3bc3d531c377d5f49a28c2547a717b355e521b7a
lauuurars <1120098177@u.icesi.edu.co>
2025-05-05 08:18:48 -0500

    fin script 02

--- Recursos/Ejercicios/script02.js
+++ Recursos/Ejercicios/script02.js
@@ -8,8 +8,15 @@
  * Abrir y cerrar el navegador para ver que pasa en el Local Storage
  *  Abrir el enlace con otro navegador ¿Que pasa?
  * */ 
- 
 
+const libro = {
+    titulo: '100 años de soledad',
+    autor: 'Gabriel Garcia Marquez',
+    nacionalidad: 'Colombiana'
+}
 
+//serializamos el objeto para almacenarlo 
 
+const libroJSON = JSON.stringify(libro) //convertimos el objeto JS a JSON
+localStorage.setItem('libro', libroJSON) //guardamos correctamente el objeto serializado
 

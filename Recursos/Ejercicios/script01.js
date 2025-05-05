629c88cad8d0995f15afbf05d8355abc0aa93f51
lauuurars <1120098177@u.icesi.edu.co>
2025-05-05 07:57:52 -0500

    fin script 01

--- Recursos/Ejercicios/script01.js
+++ Recursos/Ejercicios/script01.js
@@ -12,5 +12,43 @@
  * Limpar Todo el LocalStorage con clear()
  */
 
+//Guardamos Datos en el Local Storage usando setItem(llave,valor)
+localStorage.setItem('usuario', 'Laura')
+localStorage.setItem('edad', 18)
+localStorage.setItem('email', 'laura@gmail.com')
+localStorage.setItem('mascota', 'michi')
 
+//Recuperamos datos del Local Storage usando getItem()
+
+const usuario = localStorage.getItem('usuario') //Laura
+const edad = localStorage.getItem('edad')  //18
+const email = localStorage.getItem('email') //laura@gmail.com
+const mascota = localStorage.getItem('mascota') //michi
+
+//mostramos los datos en consola
+console.log(usuario)
+console.log(edad)
+console.log(email)
+console.log(mascota)
+
+//Actualizar la edad del usuario
+
+localStorage.setItem('edad', 19)
+
+//Ver la longitud de LocalStorage con Length
+
+console.log(localStorage.length) 
+
+//ver la posición de una llave con key(index)
+
+const llave = localStorage.key(2) //mostramos el email
+console.log(llave)
+
+//remover un valor del localStorage usando removeItem 
+
+localStorage.removeItem('mascota') //eliminamos el valor mascota
+
+//limpiamos todo el localStorage usando clear()
+
+localStorage.clear() //limpiamos todo el localStorage
 

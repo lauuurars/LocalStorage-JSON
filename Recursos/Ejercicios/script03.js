d60ce9f42805a2563f0218dceb36ce481a979c49
lauuurars <1120098177@u.icesi.edu.co>
2025-05-05 08:43:46 -0500

    fin script 03

--- Recursos/Ejercicios/script03.js
+++ Recursos/Ejercicios/script03.js
@@ -8,3 +8,23 @@
  *  Validar antes de mostrar el objeto recuperado
  *  Mostrar los datos recuperado en el contenedor html
  **/ 
+
+
+//recuperamos el objeto guardado y lo mostramos por consola
+
+const libro = JSON.parse(localStorage.getItem('libro')) //convertimos el objeto JSON a JS
+
+if (libro == null) {
+    console.log("No hay libros!!!!")
+} else {
+    console.log(libro.titulo)
+    console.log(libro.autor)
+    console.log(libro.nacionalidad)
+    const contenedor = document.querySelector('#contenedor')
+    contenedor.innerHTML = `
+        <h2>📕Libro UwU: ${libro.titulo}</h2>
+        <p>Autor: ${libro.autor}</p>
+        <p>Nacionalidad: ${libro.nacionalidad}</p>
+    `
+}
+
